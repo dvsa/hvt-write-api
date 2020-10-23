50f83c8c820a681b4d3c19bba881695bdf6ff1f8
Andy Taylor <a.taylor@kainos.com>
2020-10-23 08:34:18 +0000

    Enhance/Add dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 const AwsSamPlugin = require('aws-sam-webpack-plugin');
 
 const awsSamPlugin = new AwsSamPlugin({ vscodeDebug: false });
+const PATCH_LAMBDA_NAME = "BulkUpdateFunction"; 
+const BULK_UPDATE_LAMBDA_NAME = "PatchLambdaFunction";
 
 module.exports = {
   // Loads the entry object from the AWS::Serverless::Function resources in your
@@ -35,5 +38,13 @@ module.exports = {
   },
 
   // Add the AWS SAM Webpack plugin
-  plugins: [awsSamPlugin]
+  plugins: [
+    awsSamPlugin,
+    new CopyPlugin({
+      patterns: [
+        { from: './.env', to: `.aws-sam/build/${PATCH_LAMBDA_NAME}/` },
+        { from: './.env', to: `.aws-sam/build/${BULK_UPDATE_LAMBDA_NAME}/` },
+      ],
+    }),
+  ]
 };
